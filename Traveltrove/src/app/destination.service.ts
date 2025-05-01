f061362dd96809261716b148158a4d324873c04b
girish.jangam <girish.jangam@ad.infosys.com>
2025-05-01 18:00:55 +0530

    girish.jangam

---
+++ Traveltrove/src/app/destination.service.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { BehaviorSubject, Observable, tap } from 'rxjs';
+import { response } from 'express';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DestinationService {
+  searchDestinations(searchQuery: any) {
+    throw new Error('Method not implemented.');
+  }
+  private reviewSubject= new BehaviorSubject<any[]>([]);
+  reviews$=this.reviewSubject.asObservable();
+  baseUrl = 'http://localhost:3000/api/v1/destinationGuides';
+  baseUrl2 = 'http://localhost:3000/api/v1/destinationDetails';
+  reviewsUrl = 'http://localhost:3000/api/v1/reviews'; // Adjust the URL as needed
+
+  constructor(private http: HttpClient) {}
+
+  searchDestination(): Observable<any[]> {
+    return this.http.get<any[]>(this.baseUrl);
+  }
+
+  getDestination(id: string): Observable<any> {
+    return this.http.get(`${this.baseUrl2}/${id}`);
+  }
+
+  submitReview(destinationId: string, review: any): Observable<any> {
+    return this.http.put(`${this.reviewsUrl}/${destinationId}`, review).pipe(
+      tap((response: any) => {
+        // After submitting the review, emit the updated reviews list (assuming response.reviews contains the updated list)
+        this.reviewSubject.next(response.reviews);
+      })
+    );
+  }
+  
+  getReviews(): Observable<any[]> {
+    return this.http.get<any[]>(this.baseUrl2);
+  }
+
+
+}
